196e13b3d262e88589f9a4d19c7cc3c419c6d384
AdleeAfif <kamitokazuya@gmail.com>
2022-12-05 15:20:14 +0800

    Import RXJS, created resposive header and components.

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import Profile from './Profile/Profile'
-import Footer from './Footer/Footer'
-import './Home.css'
+import React from "react";
+import Profile from "./Profile/Profile";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="home-container">
-        <Profile/>
-        <Footer/>
+      <Header />
+      <Profile />
+      <Footer />
     </div>
-  )
+  );
 }
